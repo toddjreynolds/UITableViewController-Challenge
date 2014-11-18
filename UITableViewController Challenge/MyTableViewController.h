abe11772be0b84f614672f5f78ca24b55bf58c49
toddjreynolds <toddjreynolds@toddreynoldss-MacBook-Pro.local>
2014-11-18 09:15:20 -0700

    Removed the default view controller files and set up a UITableViewController

--- UITableViewController Challenge/MyTableViewController.h
+++ UITableViewController Challenge/MyTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyTableViewController.h
 //  UITableViewController Challenge
 //
 //  Created by Todd Reynolds on 11/18/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyTableViewController : UITableViewController
 
 @end
-
