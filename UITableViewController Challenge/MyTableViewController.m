3b03c2e52612ced0bcdbddc838176e53acd8cb1b
Todd Toddandbrooke Brooke Reynolds <toddandbrooke@Our-New-iMac.local>
2014-11-19 06:51:05 -0700

    Configured MyTableViewController.m and it's corresponding Storyboard.

--- UITableViewController Challenge/MyTableViewController.m
+++ UITableViewController Challenge/MyTableViewController.m
@@ -32,26 +32,40 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if (section == 0 ) {
+        return 2;
+    }
+    else if (section == 1) {
+        return 1;
+    }
+    else {
+        return 3;
+    }
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"Cell" forIndexPath:indexPath];
     
     // Configure the cell...
-    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"I am in Section 0";
+    }
+    else if (indexPath.section == 1) {
+        cell.textLabel.text = @"Another section";
+    }
+    else {
+        cell.textLabel.text = [NSString stringWithFormat:@"This is cell %ld of section %i", (long)indexPath.row, indexPath.section];
+    }
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
